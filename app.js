4adfcb21a5d9f8b9b9754d3c82e46c936c9a4a8b
PenuelDaimari <phd0692@gmail.com>
2024-12-10 21:15:18 +0530

    created the first version of playable tic tac toe

---
+++ app.js
@@ -0,0 +1,99 @@
+let cells = document.querySelectorAll(".cell");
+let newGame = document.querySelector("#reset");
+let finalWinner = document.querySelector("#result");
+let butt = document.querySelector("button")
+
+let count = 0;
+let values = [];
+for(let i = 0;i<9;i++){
+    values.push('');
+}
+let uniq = [];
+let winner = "tie";
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+const checkWinner = () => {
+    let result = false;
+    for(let pattern of winPatterns){
+        val1 = pattern[0];
+        val2 = pattern[1];
+        val3 = pattern[2];
+        if((values[val1]!='')&&(values[val1]===values[val2] && values[val2]===values[val3])){
+            result = true;
+        }
+    }
+    return result;
+}
+
+for(let i = 0;i<9;i++){
+    uniq.push('0');
+}
+
+newGame.addEventListener('click',()=>{
+    for(let n = 0;n<9;n++){
+        cells[n].innerHTML="";
+        uniq[n] = 0;
+        values[n]='';
+    }
+    count = 0;
+    finalWinner.innerHTML="";
+    finalWinner.style.visibility = "hidden";
+    finalWinner.style.color = "#84cdee";
+    winner = "tie";
+    heading.classList.remove("errenv");
+    butt.classList.remove("buttonErr");
+})
+
+
+for(let i=0;i<9;i++){
+    cells[i].addEventListener('click',()=>{
+        if(uniq[i]==0){
+            count++;
+            console.log(`${i}`)
+            if(count%2==0){
+                cells[i].innerHTML = "<h1 class = 'xo'>O</h1>";
+                values[i]='o';
+                let result = checkWinner();
+                if(result==true){
+                    for(let j = 0;j<9;j++){
+                        uniq[j]=1;
+                    }
+                    winner = 'o';
+                    finalWinner.innerHTML="<i>Team O won!</i>";
+                    finalWinner.style.visibility = "visible";
+                    console.log("Game over")
+                }
+            }
+            else{
+                cells[i].innerHTML = "<h1 class = 'xo'>X<h1>";
+                values[i]='x';
+                let result = checkWinner();
+                if(result==true){
+                    for(let j = 0;j<9;j++){
+                        uniq[j]=1;
+                    }
+                    winner = 'x';
+                    finalWinner.innerHTML="<i>Team X won!</i>";
+                    finalWinner.style.visibility = "visible";
+                    console.log("Game over");
+                }
+            }
+            uniq[i]=1;
+        }
+        if(count==9 && winner=="tie"){
+            finalWinner.innerHTML="<i>What a Tough Competition!</i>";
+            finalWinner.style.visibility = "visible";
+            finalWinner.style.color = "#84cdee";
+            console.log("Game over")
+        }
+    })
+}
